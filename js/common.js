c7a3a285c17cb52877bed57849572a933f462c24
zhu <208895638@qq.com>
2017-12-20 19:08:39 +0800

    页面布局修改

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function(){
+//	$(window).on('scroll',function(){
+//      var top = $(window).scrollTop();
+//      if(top >= 142){
+//         $(".lBox").addClass("fix");
+//         $(".rBox").css("left","240px")
+//      }else{
+//          $(".lBox").removeClass("fix");
+//         	$(".rBox").css("left","0")
+//      };
+//  });
+})
