3aec8424e07514f1516e71f0f3c0e260a8653600
zhu <208895638@qq.com>
2017-12-22 17:49:35 +0800

    1

--- js/common.js
+++ js/common.js
@@ -9,4 +9,47 @@ $(function(){
 //         	$(".rBox").css("left","0")
 //      };
 //  });
+//顶部消息通知划过展开
+	$(".tongzhi").hover(function(){
+		if(!$(".tongzhi .newsInfo").is(":animated")){
+			$(".tongzhi .newsInfo").stop(true,true).slideDown();
+		}
+	},function(){
+		$(".tongzhi .newsInfo").stop(true,true).slideUp();
+	});
+	select($(".khbh"));  //我的下级查询客户编号
+	select($(".khjb"));  //我的下级客户级别
+	
+//	function select(obj){
+//		//控制select框的缩放
+//		obj.on("click",function(){
+//			$(this).find(".option").slideToggle();
+//		});
+//		//选取点击的值
+//		obj.find(".option").on("click","li",function(){
+//			var text = $(this).html();
+//			obj.find("em").html(text);
+//		});
+//		//移出时box缩进
+//		obj.mouseleave(function(){
+//			$(this).find(".option").slideUp();
+//		});
+//	};
 })
+function select(obj){
+
+		//控制select框的缩放
+		obj.on("click",function(){
+			console.log(obj)
+			$(this).find(".option").slideToggle();
+		});
+		//选取点击的值
+		obj.find(".option").on("click","li",function(){
+			var text = $(this).html();
+			obj.find("em").html(text);
+		});
+		//移出时box缩进
+		obj.mouseleave(function(){
+			$(this).find(".option").slideUp();
+		});
+	};
