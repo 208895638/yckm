ac3c81a54d9ae25438820a8e607c5a4e26708208
zhu <208895638@qq.com>
2017-12-25 18:58:35 +0800

    基本完成 剩下一些细节没做 明天做 今天12.25

--- js/common.js
+++ js/common.js
@@ -1,14 +1,4 @@
 $(function(){
-//	$(window).on('scroll',function(){
-//      var top = $(window).scrollTop();
-//      if(top >= 142){
-//         $(".lBox").addClass("fix");
-//         $(".rBox").css("left","240px")
-//      }else{
-//          $(".lBox").removeClass("fix");
-//         	$(".rBox").css("left","0")
-//      };
-//  });
 //顶部消息通知划过展开
 	$(".tongzhi").hover(function(){
 		if(!$(".tongzhi .newsInfo").is(":animated")){
@@ -19,37 +9,50 @@ $(function(){
 	});
 	select($(".khbh"));  //我的下级查询客户编号
 	select($(".khjb"));  //我的下级客户级别
-	
-//	function select(obj){
-//		//控制select框的缩放
-//		obj.on("click",function(){
-//			$(this).find(".option").slideToggle();
-//		});
-//		//选取点击的值
-//		obj.find(".option").on("click","li",function(){
-//			var text = $(this).html();
-//			obj.find("em").html(text);
-//		});
-//		//移出时box缩进
-//		obj.mouseleave(function(){
-//			$(this).find(".option").slideUp();
-//		});
-//	};
-})
-function select(obj){
+	//td里面下拉框这部分是单独写的
+	$(".subordinateInfo td .select").on("click",function(){
+		$(this).find(".option").slideToggle();
+	});
+	$(".subordinateInfo td .select").find(".option").on("click","li",function(){
+		var text = $(this).html();
+		$(this).parent().siblings().find("em").html(text);
+	});
+	//移出时box缩进
+	$(".subordinateInfo td .select").mouseleave(function(){
+		$(this).find(".option").slideUp();
+	});
+	//轮播广告
+	var timer;
+	adv(".advBox",2000);
+});
 
+function adv(obj,s){
+	var height = $(obj).height();
+	var perH = $(obj).find("li").eq(0).height();
+	timer=setInterval(function(){
+		var top = parseInt($(obj).css("margin-top"));
+		$(obj).animate({"margin-top":top-perH},function(){
+			if(Math.abs(parseInt($(obj).css("margin-top")))>height - perH){
+				$(obj).css({"margin-top":0});
+			};
+		})
+	},s);
+};
+
+function select(obj){
 		//控制select框的缩放
-		obj.on("click",function(){
-			console.log(obj)
-			$(this).find(".option").slideToggle();
-		});
-		//选取点击的值
-		obj.find(".option").on("click","li",function(){
-			var text = $(this).html();
-			obj.find("em").html(text);
-		});
-		//移出时box缩进
-		obj.mouseleave(function(){
-			$(this).find(".option").slideUp();
-		});
-	};
+		console.log(obj)
+	obj.on("click",function(){
+		console.log(obj)
+		$(this).find(".option").slideToggle();
+	});
+	//选取点击的值
+	obj.find(".option").on("click","li",function(){
+		var text = $(this).html();
+		obj.find("em").html(text);
+	});
+	//移出时box缩进
+	obj.mouseleave(function(){
+		$(this).find(".option").slideUp();
+	});
+};
