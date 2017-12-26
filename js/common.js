6a7aa7bdf725ad063968580b8b3a68d3136bbda9
zhu <208895638@qq.com>
2017-12-26 18:58:59 +0800

    优化

--- js/common.js
+++ js/common.js
@@ -24,11 +24,43 @@ $(function(){
 	//轮播广告
 	var timer;
 	adv(".advBox",2000);
+	mouseEvent(".advBox",2000)
+	//公共选择时间
+	var data,year,month,day;
+	data = new Date();
+	year = data.getFullYear();
+	month = data.getMonth() + 1;
+	day = data.getDate();
+	var dateRange1 = new pickerDateRange('data', {
+    	aRecent7Days: 'aRecent7Days',	
+        isTodayValid: true,
+        startDate: year+"-"+(month-1)+"-"+day,
+        endDate: year+"-"+month+"-"+day,
+        startToday: false,
+        stopToday: true,
+        needCompare: false,
+        defaultText: ' ~ ',
+        autoSubmit: false,
+        inputTrigger: 'input_trigger1',
+        theme: 'ta',
+        success: function (obj) {
+            $("#dCon2").html('开始时间 : ' + obj.startDate + '<br/>结束时间 : ' + obj.endDate);
+        }
+    });
+	//公共选择时间按钮组
+	$(".daysOfBox button").on("click",function(){
+		$(this).addClass("on").siblings().removeClass("on");
+	});
+	//tab栏切换
+	$(".tab ul li").on("click",function(){
+		$(this).addClass("on").siblings().removeClass("on");
+	})
 });
-
+// 广告轮播
 function adv(obj,s){
 	var height = $(obj).height();
 	var perH = $(obj).find("li").eq(0).height();
+	var tops;//记录鼠标移入广告时的margintop值
 	timer=setInterval(function(){
 		var top = parseInt($(obj).css("margin-top"));
 		$(obj).animate({"margin-top":top-perH},function(){
@@ -38,7 +70,16 @@ function adv(obj,s){
 		})
 	},s);
 };
-
+//鼠标进入广告和鼠标移出广告
+function mouseEvent(obj,s){
+	$(obj).mouseenter(function(){
+		clearInterval(timer);
+	});
+	$(obj).mouseleave(function(){
+		adv(".advBox",s);
+	});
+}
+//模拟下拉框
 function select(obj){
 		//控制select框的缩放
 		console.log(obj)
